27f9565dac1ecf658a768a556db1accd86343cd3
Lalita Shahi <shahilalita616@gmail.com>
2025-09-20 23:43:48 +0200

    init commit

---
+++ src/main/java/se/lexicon/ws_springboot_jpa_solutiong77/WsSpringbootJpaSolutionG77Application.java
@@ -0,0 +1,13 @@
+package se.lexicon.ws_springboot_jpa_solutiong77;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsSpringbootJpaSolutionG77Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsSpringbootJpaSolutionG77Application.class, args);
+    }
+
+}
